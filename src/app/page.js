4244ad8304289aab4d34b190cc73a246968e9c47
giansegg <gianpier.segovia@utec.edu.pe>
2024-05-24 21:23:31 -0500

    c1

--- src/app/page.js
+++ src/app/page.js
@@ -81,14 +81,12 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+            MI BRO{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Hola</p>
         </a>
 
         <a
